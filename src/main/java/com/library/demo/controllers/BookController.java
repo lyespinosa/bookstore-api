4de999a0666329aef25baec5ecd0e2b8b32caff6
lyespinosa <leonardoyahelespinosa@gmail.com>
2022-10-24 11:32:40 -0500

    add lisBook

--- src/main/java/com/library/demo/controllers/BookController.java
+++ src/main/java/com/library/demo/controllers/BookController.java
@@ -1,4 +1,23 @@
 package com.library.demo.controllers;
 
+import com.library.demo.controllers.dtos.responses.BaseResponse;
+import com.library.demo.services.interfaces.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("book")
 public class BookController {
+
+    @Autowired
+    private IBookService service;
+
+    @GetMapping("")
+    public ResponseEntity<BaseResponse> listBooks(){
+        BaseResponse baseResponse = service.listBooks();
+        return new ResponseEntity<>(baseResponse, baseResponse.getHttpStatus());
+    }
 }
