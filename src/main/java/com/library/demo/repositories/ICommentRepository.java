81434130f47423f26236d328769c4c4479d15f4d
Darinel080903 <darinelmr12345@gmail.com>
2022-10-31 14:06:25 -0600

    Service and Repository added

--- src/main/java/com/library/demo/repositories/ICommentRepository.java
+++ src/main/java/com/library/demo/repositories/ICommentRepository.java
@@ -1,9 +1,14 @@
 package com.library.demo.repositories;
 
 import com.library.demo.entities.Comment;
+import com.library.demo.entities.projections.CommentProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query(value = "select comments.*",nativeQuery = true)
+    CommentProjection getCommentById(Long commentId);
 }
--- src/main/java/com/library/demo/services/CommentServiceImpl.java
+++ src/main/java/com/library/demo/services/CommentServiceImpl.java
@@ -1,4 +1,45 @@
 package com.library.demo.services;
 
-public class CommentServiceImpl {
+import com.library.demo.controllers.dtos.requests.CommentRequest;
+import com.library.demo.controllers.dtos.responses.BaseResponse;
+import com.library.demo.controllers.dtos.responses.CommentResponse;
+import com.library.demo.entities.Comment;
+import com.library.demo.repositories.ICommentRepository;
+import com.library.demo.services.interfaces.ICommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentServiceImpl implements ICommentService {
+
+    @Autowired
+    private ICommentRepository repository;
+
+    @Override
+    public BaseResponse listComments() {
+        List<CommentResponse> responses = repository.findAll().stream().map(this::from).collect(Collectors.toList());
+        return BaseResponse.builder()
+                .data(responses)
+                .message("Comments list")
+                .success(Boolean.TRUE)
+                .httpStatus(HttpStatus.OK)
+                .build();
+    }
+
+    @Override
+    public BaseResponse addComment(CommentRequest request) {
+        return null;
+    }
+
+    private CommentResponse from(Comment comment) {
+        CommentResponse response = new CommentResponse();
+        response.setId(comment.getId());
+        response.setId(comment.getId());
+        return response;
+    }
+
 }
